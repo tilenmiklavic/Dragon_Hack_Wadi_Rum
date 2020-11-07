bb697586d95f5fd6bc0456cc304e093dbadb8746
mtx13 <matepeklaj@gmail.com>
2020-11-07 23:16:24 +0100

    graf

--- bin/public/static/script/script.js
+++ bin/public/static/script/script.js
@@ -8,32 +8,28 @@ window.onload = function() {
         console.log(e);
     });
     */
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var chart = new Chart(ctx, {
-        type: 'doughnut',
-
-        data: {
-            datasets: [{
-                label: 'My First dataset',
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data: [60, 10],
-                backgroundColor: ['#FFC0CB', '#FFFFFF'],
-                borderWidth: 5
-            }]
-        },
-
-
-        // Configuration options go here
-        options: {}
-    });
-
     document.getElementById("procent").addEventListener("change", uporabi());
 }
 
 function uporabi() {
     procent = document.getElementById("procent").value
     prob = document.getElementById("verjetnost").value
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        type: 'doughnut',
+
+        data: {
+            datasets: [{
+                label: 'Procent',
+                backgroundColor: '#6EBEC4',
+                data: [(1-procent)*100, procent*100],
+                backgroundColor: ['#6EBEC4', '#FFFFFF'],
+                borderWidth: 5
+            }]
+        },
+        // Configuration options go here
+        options: {}
+    });
     console.log(procent)
     console.log(prob)
 }
